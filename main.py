1d8058a0afdad08ed82d98fef25bc88bf2760643
disugiske <disugiske@yandex.ru>
2022-11-07 19:57:42 +0300

    test

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-import httpx
+
 from fastapi import FastAPI, Request
 from starlette.responses import JSONResponse
 
 app = FastAPI()
 @app.get("/", response_class=JSONResponse)
 def weather(request: Request):
-    #response = httpx.get()
     client_host = request.client.host
     return JSONResponse(client_host)

425bd95dcc3c0efab6c9915f58e13240c39140d2
disugiske <disugiske@yandex.ru>
2022-11-07 20:58:48 +0300

    add logger

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
+import logging
+import sys
 from fastapi import FastAPI, Request
 from starlette.responses import JSONResponse
 from utils import ip_to_loc, openweather
+
+logger = logging.getLogger('Weather')
+
+logging.basicConfig(
+        filename="weather.log",
+        encoding="UTF-8",
+        filemode="w",
+        format="[%(asctime)s] %(levelname).1s %(message)s",
+        datefmt="%d-%b-%y %H:%M:%S",
+        level= logging.INFO
+    )
+handler = logging.StreamHandler(stream=sys.stdout)
+handler.setFormatter(logging.Formatter("[%(asctime)s] %(levelname).1s %(message)s"))
+logger.addHandler(handler)
 
 app = FastAPI()
 @app.get("/", response_class=JSONResponse)
--- utils.py
+++ utils.py
@@ -1,14 +1,18 @@
 import os
-
 import httpx
+from main import logger
+from dotenv import load_dotenv
 
+load_dotenv()
 
 async def ip_to_loc(ip):
     response = httpx.get(f"https://ipinfo.io/{ip}?token={os.environ.get('IPINFO_TOKEN')}").json()
+    logger.info(response)
     loc = response.get('loc')
     lat, lon = loc.split(',')
     return lat, lon
 
 async def openweather(lat, lon):
     weather_response = httpx.get(f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={os.environ.get('API_WEATHER_KEY')}")
+    logger.info(weather_response)
     return weather_response.json()
